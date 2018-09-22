18e4895d41d4c21148ea0d15ceb8d8edd6eb9ac1
DamianVogel <rodolfovogel@gmail.com>
2018-09-22 20:45:22 -0300

    Se suman componentes y se prueba input / output

--- EjerciciosClase/src/app/app.module.ts
+++ EjerciciosClase/src/app/app.module.ts
@@ -10,14 +10,31 @@ import { HttpModule } from '@angular/http';
 
 import { MiHttpService } from './servicios/mi-http.service';
 import { GenericoService } from './servicios/generico.service';
+import { TemaComponent } from './componentes/tema/tema.component';
+import { CuestionarioComponent } from './componentes/cuestionario/cuestionario.component';
+import { PreguntaComponent } from './componentes/pregunta/pregunta.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';    
+
+
+const miRuteo = [{path:'pruebaRuteo', component:PreguntaComponent}]
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemaComponent,
+    CuestionarioComponent,
+    PreguntaComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(miRuteo)
+
     //HttpClientModule
     
   ],
---
+++ EjerciciosClase/src/app/componentes/pregunta/pregunta.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-pregunta',
+  templateUrl: './pregunta.component.html',
+  styleUrls: ['./pregunta.component.css']
+})
+export class PreguntaComponent implements OnInit {
+
+  tema: string;
+  
+  @Input()
+  
+  cuestionario: string;
+
+  
+  @Output() emiterPregunta:EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  funcionContador()
+  {
+    this.emiterPregunta.emit();
+  }
+
+
+}
